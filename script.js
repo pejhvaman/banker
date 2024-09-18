b82ac3ca510906578065fa79413cae4d0a341825
pejhvaman <pejhvaman@gmail.com>
2024-09-19 00:48:22 +0330

    movements dates and current date

--- script.js
+++ script.js
@@ -3,33 +3,45 @@
 // Data
 const account1 = {
   owner: "Pezhman Yazdankhah",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.3, // %
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2023-11-18T21:31:17.178Z",
+    "2023-12-23T07:42:02.383Z",
+    "2024-01-28T09:15:04.904Z",
+    "2024-09-11T10:17:24.185Z",
+    "2024-09-16T14:11:59.604Z",
+    "2024-09-17T17:01:17.194Z",
+    "2024-09-18T18:36:17.929Z",
+    "2024-09-19T00:30:36.790Z",
+  ],
+  currency: "IRR",
+  locale: "fa-IR", // de-DE
 };
 
 const account2 = {
   owner: "Ansar Mirzayi",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.1,
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Mehran Hamedi",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.9,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "M.Hossein Rahmani",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const enterEL = document.querySelector(".enter"),
@@ -56,7 +68,8 @@ const enterEL = document.querySelector(".enter"),
   closeUserInput = document.getElementById("user-to-close"),
   closePassInput = document.getElementById("pass-to-close"),
   closeAccBtn = document.querySelector(".close-btn"),
-  sortBtn = document.querySelector(".sort");
+  sortBtn = document.querySelector(".sort"),
+  currentDateEl = document.querySelector(".current-date");
 // Important Variables
 let currentAccount,
   sort = false;
@@ -120,6 +133,32 @@ const clearBlurInputs = function (...inputs) {
   });
 };
 
+const simpleDater = (dateObj) => {
+  const year = dateObj.getFullYear();
+  const month = `${dateObj.getMonth() + 1}`.padStart(2, 0);
+  const day = `${dateObj.getDate()}`.padStart(2, 0);
+  return `${year}/${month}/${day}`;
+};
+
+const displayCurrentDate = () => {
+  const date = simpleDater(new Date());
+  currentDateEl.textContent = date;
+};
+
+displayCurrentDate();
+
+const calcDaysPassed = (date1, date2) =>
+  Math.round(Math.abs(date1 - date2) / (24 * 60 * 60 * 1000));
+
+const movmentDateString = (date) => {
+  const day = calcDaysPassed(date, new Date());
+  if (day === 0) return "today";
+  if (day === 1) return "yesterday";
+  if (day > 1 && day < 7) return `${day} days ago`;
+  if (day === 7) return "a week ago";
+  else return simpleDater(date);
+};
+
 const showMovements = function (currentAccount, sort = false) {
   movementsEl.innerHTML = "";
 
@@ -127,19 +166,25 @@ const showMovements = function (currentAccount, sort = false) {
 
   const movsMap = new Map();
 
-  movsArr.forEach((mov, i) => movsMap.set(mov, i + 1));
+  movsArr.forEach((mov, i) => movsMap.set(mov, i));
 
   const sortedMovs = movsArr.slice().sort((a, b) => a - b);
 
   const movements = sort ? sortedMovs : movsArr;
 
+  //date
+  const movDates = currentAccount.movementsDates;
+
   movements.forEach((mov) => {
     const movRow = `
             <li class="transaction ${mov > 0 ? "deposit" : "withdrawal"}">
-              <span class="transaction-label">${movsMap.get(mov)} ${
+              <span class="transaction-label">${movsMap.get(mov) + 1} ${
       mov > 0 ? "deposit" : "withdrawal"
     }</span>
-              <span class="transaction-value">${mov} $</span>
+              <span class="transaction-date">${movmentDateString(
+                new Date(movDates[movsMap.get(mov)])
+              )}</span>
+              <span class="transaction-value">${mov.toFixed(2)} $</span>
             </li>
         `;
     movementsEl.insertAdjacentHTML("afterbegin", movRow);
@@ -155,17 +200,17 @@ const calcDisplaySummary = function (currentAccount) {
   const income = currentAccount.movements
     .filter((mov) => mov > 0)
     .reduce((inc, cur) => inc + cur, 0);
-  incomeEl.textContent = `${income} $`;
+  incomeEl.textContent = `${income.toFixed(2)} $`;
   const outgo = currentAccount.movements
     .filter((mov) => mov < 0)
     .reduce((out, cur) => out + cur, 0);
-  outgoEl.textContent = `${Math.abs(outgo)} $`;
+  outgoEl.textContent = `${Math.abs(outgo).toFixed(2)} $`;
   const interest = currentAccount.movements
     .filter((mov) => mov > 0)
     .map((dep) => (dep * currentAccount.interestRate) / 100)
     .filter((int) => int >= 1)
     .reduce((int, cur) => int + cur, 0);
-  interestEl.textContent = `${interest.toFixed(4)} $`;
+  interestEl.textContent = `${interest.toFixed(2)} $`;
 };
 
 const updateUI = function (currentAccount) {
